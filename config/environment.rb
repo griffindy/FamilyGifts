c9f9ff94d5c215e25c9436511201417237ac4f9b
Dylan Griffin <griffindy@gmail.com>
2011-12-26 17:27:43 -0500

    allows for the 'birthday' format of the date to be used

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FamilyGifts::Application.initialize!
+
+# extra datetime output
+Time::DATE_FORMATS[:birthday] = "%B %d, %Y"
